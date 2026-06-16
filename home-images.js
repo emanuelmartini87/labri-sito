e7c7e8bc8924ce56917a4bb9d40a68f82c7dc803
emanuelmartini87 <emanuel.martini87@gmail.com>
2026-06-16 15:20:29 +0200

    Create home-images.js

---
+++ home-images.js
@@ -0,0 +1,12 @@
+const homeImages = [
+  "insegna.jpg",
+  "immagini/Home/Colazione1.jpg",
+  "immagini/Home/Colazione2.jpg",
+  "immagini/Home/GarageMoto.jpg",
+  "immagini/Home/Giardino1.jpg",
+  "immagini/Home/Giardino2.jpg",
+  "immagini/Home/Giardino3.jpg",
+  "immagini/Home/Giardino4.jpg",
+  "immagini/Home/Giardino5.jpg",
+  "immagini/Home/Ingresso1.jpg"
+];
